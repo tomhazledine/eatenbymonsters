704e3acf6e70d499985173b3b75c178e14281e8b
DWM <dwm@Marks-MacBook-Air-272.local>
2015-05-17 17:31:32 +0100

    testing amplitude

--- assets/js/app.js
+++ assets/js/app.js
@@ -10312,7 +10312,7 @@ var rawAudioData = $('#audioData');
 
 if (audioWrapper.length && rawAudioData.length) {
   console.log('There is audio on this page!');
-  console.log(stuff);
+  // console.log();
   // var audioData = rawAudioData.innerHTML;
   // console.log(audioData);
   // audiDataJson = JSON.parse(audioData)
