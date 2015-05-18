3ff6256e5fa6ef0272edd3e466dd2da8b47bdff5
DWM <dwm@Marks-MacBook-Air-272.local>
2015-05-18 08:33:21 +0100

    basic audio player

--- assets/js/app.js
+++ assets/js/app.js
@@ -10310,14 +10310,74 @@ return jQuery;
 var audioWrapper = $('#audio');
 var rawAudioData = $('#audioData');
 
+var songPlayPause = $('.playlistSongTrigger');
+
+var myAudio = [];
+
 if (audioWrapper.length && rawAudioData.length) {
-  console.log('There is audio on this page!');
-  // console.log();
-  // var audioData = rawAudioData.innerHTML;
-  // console.log(audioData);
-  // audiDataJson = JSON.parse(audioData)
-  // console.log(audiDataJson);
+  
+  for (var i = 0; i < songs.length; i++) {
+    myAudio[i] = new Audio(songs[i].url);
+  };
+
+
+
 };
+
+songPlayPause.on('click',function(){
+  var clickedButton = $(this);
+  var songIndex = clickedButton.data('song-index');
+  console.log(songIndex);
+  console.log(myAudio[songIndex]);
+  // myAudio[songIndex].play();
+
+  if (clickedButton.hasClass('songPlaying')) {
+    console.log('Pause!');
+    myAudio[songIndex].pause();
+    clickedButton.removeClass('songPlaying');
+    clickedButton.addClass('songPaused');
+  } else {
+    console.log('Play!');
+    songPlayPause.removeClass('songPlaying');
+    songPlayPause.addClass('songPaused');
+    playSong(songIndex,clickedButton);
+    clickedButton.removeClass('songPaused');
+    clickedButton.addClass('songPlaying');
+  }
+
+});
+
+function playSong(index,button){
+  // stop all songs playing
+  for (var i = 0; i < songs.length; i++) {
+    if (i != index) {
+      myAudio[i].pause();
+    };
+  };
+  // Play Target Song
+  myAudio[index].play();
+  // If it's the first time the song has been played,
+  // get the song's length
+  if (button.hasClass('notPlayedYet')) {
+    var songLength = myAudio[index].duration;
+    //console.log(songLength);
+    var mmss = songLength / 60;
+    mmss = mmss.toFixed(2);
+
+    var songClass = '.song' + index;
+    var songLengthBox = $(songClass).find('.songDuration');
+    songLengthBox.html(mmss);
+
+    // secondsToMMSS(songLength);
+    // console.log(mmss);
+    button.removeClass('notPlayedYet');
+  };
+}
+
+function secondsToMMSS(seconds){
+  var mmss = seconds / 60;
+  return mmss;
+}
 
 var mobileNavToggle = $('#mobileNavToggle');
 var mainMenu = $('#mainMenu');
