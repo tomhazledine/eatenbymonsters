bdeb3e3dacbc2c47957b5f63c7dbf97816782f4e
tom <tom@wearedwm.com>
2015-06-12 07:35:20 +0100

    no jQuery audio playPause function

--- _uncompressed/js/custom/audioPlugin.js
+++ _uncompressed/js/custom/audioPlugin.js
@@ -8,16 +8,90 @@ var songsData = songs;
 // console.log(songs);
 
 function AudioPlayer(songData,playerWrapper){
+    
     var playPauseButtons = playerWrapper.getElementsByClassName('playlistSongTrigger');
-    // console.log(playPauseButtons);
+    var currentSongIndex = false;
+    var myAudio = [];
+    
+    // Setup event listeners
     for (i = 0; i < playPauseButtons.length; i++) {
         playPauseButtons[i].addEventListener('click',_playPauseAudio,false);
     }
 
+    // Initialise Audio
+    for (var i = 0; i < songData.length; i++) {
+        myAudio[i] = new Audio(songData[i].url);
+    }
+
     function _playPauseAudio(){
         var targetSong = this.getAttribute('data-song-index');
-        console.log(targetSong);
+        // playSong(targetSong);
+        if (_hasClass(this,'songPlaying')) {
+            pauseAll();
+            _removeClass(this,'songPlaying');
+        } else {
+            for (i = 0; i < playPauseButtons.length; i++) {
+                _removeClass(playPauseButtons[i], 'songPlaying');
+            }
+            playSong(targetSong);
+            _addClass(this,'songPlaying');
+        }
+
+
     }
+
+    function pauseAll(){
+        for (var i = 0; i < songs.length; i++) {
+            myAudio[i].pause();
+        }
+    }
+
+    function playSong(index){
+        // console.log(index);
+        // console.log(myAudio);
+        
+        currentSongIndex = index;
+        for (var i = 0; i < songs.length; i++) {
+            // console.log(myAudio[i]);
+            if (i != index) {
+                myAudio[i].pause();
+            }
+        }
+        myAudio[index].play();
+    }
+
+    // Simulate jQuery helpers
+    
+    // hasClass
+    function _hasClass(el, className){
+        if (el.classList) {
+            var result = el.classList.contains(className);
+        } else {
+            var result = new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+        }
+        return result;
+    }
+
+    // addClass
+    function _addClass(el, className){
+        if (el.classList) {
+            el.classList.add(className);
+        }
+        else {
+            el.className += ' ' + className;
+        }
+    }
+
+    // removeClass
+    function _removeClass(el, className){
+        if (el.classList) {
+            el.classList.remove(className);
+        }
+        else {
+            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+    }
+
 }
 
 AudioPlayer(songsData,playerWrapper);
--- _uncompressed/js/custom/customAudio.js
+++ _uncompressed/js/custom/customAudio.js
@@ -1,124 +1,124 @@
-var audioWrapper = $('#audio');
-var rawAudioData = $('#audioData');
+// var audioWrapper = $('#audio');
+// var rawAudioData = $('#audioData');
 
-var songPlayPause = $('.playlistSongTrigger');
+// var songPlayPause = $('.playlistSongTrigger');
 
-var currentSongIndex = false;
-var myAudio = [];
+// var currentSongIndex = false;
+// var myAudio = [];
 
-if (audioWrapper.length && rawAudioData.length) {
+// if (audioWrapper.length && rawAudioData.length) {
   
-  for (var i = 0; i < songs.length; i++) {
-    myAudio[i] = new Audio(songs[i].url);
-  };
+//   for (var i = 0; i < songs.length; i++) {
+//     myAudio[i] = new Audio(songs[i].url);
+//   };
 
-  getUpdates();
+//   getUpdates();
 
-};
+// };
 
-songPlayPause.on('click',function(){
-  var clickedButton = $(this);
-  var songIndex = clickedButton.data('song-index');
-  // console.log(songIndex);
-  // console.log(myAudio[songIndex]);
-  // myAudio[songIndex].play();
+// songPlayPause.on('click',function(){
+//   var clickedButton = $(this);
+//   var songIndex = clickedButton.data('song-index');
+//   // console.log(songIndex);
+//   // console.log(myAudio[songIndex]);
+//   // myAudio[songIndex].play();
 
-  if (clickedButton.hasClass('songPlaying')) {
-    // console.log('Pause!');
-    myAudio[songIndex].pause();
-    clickedButton.removeClass('songPlaying');
-    clickedButton.addClass('songPaused');
-  } else {
-    // console.log('Play!');
-    songPlayPause.removeClass('songPlaying');
-    songPlayPause.addClass('songPaused');
-    playSong(songIndex,clickedButton);
-    clickedButton.removeClass('songPaused');
-    clickedButton.addClass('songPlaying');
-    console.log(currentSongIndex);
-  }
+//   if (clickedButton.hasClass('songPlaying')) {
+//     // console.log('Pause!');
+//     myAudio[songIndex].pause();
+//     clickedButton.removeClass('songPlaying');
+//     clickedButton.addClass('songPaused');
+//   } else {
+//     // console.log('Play!');
+//     songPlayPause.removeClass('songPlaying');
+//     songPlayPause.addClass('songPaused');
+//     playSong(songIndex,clickedButton);
+//     clickedButton.removeClass('songPaused');
+//     clickedButton.addClass('songPlaying');
+//     console.log(currentSongIndex);
+//   }
 
-});
+// });
 
-function playSong(index,button){
-  // stop all songs playing
-  currentSongIndex = index;
-  for (var i = 0; i < songs.length; i++) {
-    if (i != index) {
-      myAudio[i].pause();
-    };
-  };
-  // Play Target Song
-  myAudio[index].play();
-  // If it's the first time the song has been played,
-  // get the song's length
-  if (button.hasClass('notPlayedYet')) {
-    var songLength = myAudio[index].duration;
-    //console.log(songLength);
-    // var mmss = songLength / 60;
-    // mmss = mmss.toFixed(2);
-    var duration = secondsToMMSS(songLength);
+// function playSong(index,button){
+//   // stop all songs playing
+//   currentSongIndex = index;
+//   for (var i = 0; i < songs.length; i++) {
+//     if (i != index) {
+//       myAudio[i].pause();
+//     };
+//   };
+//   // Play Target Song
+//   myAudio[index].play();
+//   // If it's the first time the song has been played,
+//   // get the song's length
+//   if (button.hasClass('notPlayedYet')) {
+//     var songLength = myAudio[index].duration;
+//     //console.log(songLength);
+//     // var mmss = songLength / 60;
+//     // mmss = mmss.toFixed(2);
+//     var duration = secondsToMMSS(songLength);
 
-    var songClass = '.song' + index;
-    var songLengthBox = $(songClass).find('.songDuration');
-    songLengthBox.html(duration);
+//     var songClass = '.song' + index;
+//     var songLengthBox = $(songClass).find('.songDuration');
+//     songLengthBox.html(duration);
 
-    // secondsToMMSS(songLength);
-    // console.log(mmss);
-    button.removeClass('notPlayedYet');
-  };
-  // Set the event listener for progress updates
-  // myAudio[index].addEventListener('timeupdate',updateProgress(index),false);
-}
+//     // secondsToMMSS(songLength);
+//     // console.log(mmss);
+//     button.removeClass('notPlayedYet');
+//   };
+//   // Set the event listener for progress updates
+//   // myAudio[index].addEventListener('timeupdate',updateProgress(index),false);
+// }
 
-function secondsToMMSS(seconds){
-  var mins = Math.floor(seconds % 3600 / 60);
-  mins = mins.toFixed(0);
-  mins = mins.toString();
-  var secs = Math.floor(seconds % 3600 % 60);
-  secs = secs.toFixed(0);
-  secs = secs.toString();
-  if (secs < 10) {
-    secs = '0' + secs;
-  };
-  var mmss = mins + ':' + secs; 
-  // var mmss = seconds / 60;
-  // mmss = mmss / 60;
-  // mmss = mmss.toFixed(2);
-  return mmss;
-}
+// function secondsToMMSS(seconds){
+//   var mins = Math.floor(seconds % 3600 / 60);
+//   mins = mins.toFixed(0);
+//   mins = mins.toString();
+//   var secs = Math.floor(seconds % 3600 % 60);
+//   secs = secs.toFixed(0);
+//   secs = secs.toString();
+//   if (secs < 10) {
+//     secs = '0' + secs;
+//   };
+//   var mmss = mins + ':' + secs; 
+//   // var mmss = seconds / 60;
+//   // mmss = mmss / 60;
+//   // mmss = mmss.toFixed(2);
+//   return mmss;
+// }
 
-function getUpdates(){
-  for (var i = 0; i < songs.length; i++) {
-    var progress = 'progress';
-    myAudio[i].addEventListener('timeupdate', updateProgress, false);
-  };
-};
+// function getUpdates(){
+//   for (var i = 0; i < songs.length; i++) {
+//     var progress = 'progress';
+//     myAudio[i].addEventListener('timeupdate', updateProgress, false);
+//   };
+// };
 
-function updateProgress(){
-  var progressBarClass = '.song' + currentSongIndex;
-  var songElement = $(progressBarClass);
-  var playTimer = songElement.find('.songPlayTimer');
-  var progressBar = songElement.find('.songProgressSlider');
-  // console.log(progressBar);
+// function updateProgress(){
+//   var progressBarClass = '.song' + currentSongIndex;
+//   var songElement = $(progressBarClass);
+//   var playTimer = songElement.find('.songPlayTimer');
+//   var progressBar = songElement.find('.songProgressSlider');
+//   // console.log(progressBar);
 
-  var progress = myAudio[currentSongIndex].currentTime;
-  var duration = myAudio[currentSongIndex].duration;
-  progressParsed = secondsToMMSS(progress);
-  playTimer.html(progressParsed);
+//   var progress = myAudio[currentSongIndex].currentTime;
+//   var duration = myAudio[currentSongIndex].duration;
+//   progressParsed = secondsToMMSS(progress);
+//   playTimer.html(progressParsed);
 
-  var progressPercent = (progress / duration * 100).toFixed(2);
+//   var progressPercent = (progress / duration * 100).toFixed(2);
 
-  progressBar.val(progressPercent);
+//   progressBar.val(progressPercent);
 
-  // console.log(progressPercent);
-  // console.log(currentSongIndex);
-}
+//   // console.log(progressPercent);
+//   // console.log(currentSongIndex);
+// }
 
-function sliderScrub(newPosition,index){
-  // var what = index;
-  var duration = myAudio[index].duration;
-  var targetTime = duration * (newPosition / 100);
-  myAudio[index].currentTime = targetTime;
-  // console.log(what);
-}
+// function sliderScrub(newPosition,index){
+//   // var what = index;
+//   var duration = myAudio[index].duration;
+//   var targetTime = duration * (newPosition / 100);
+//   myAudio[index].currentTime = targetTime;
+//   // console.log(what);
+// }
